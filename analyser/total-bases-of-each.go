54e7e69187e55e9d35245c0b215a824a44653d62
iktefish <87042742+iktefish@users.noreply.github.com>
2022-04-23 16:44:37 +0600

    Implement total-bases-of-each.go

---
+++ analyser/total-bases-of-each.go
@@ -0,0 +1,43 @@
+/* TODO:
+
+- [ ] Take entire fire as arg
+- [ ] Echo From x to y segments of genome
+- [ ] Length of genome
+
+*/
+
+package analyser
+
+import (
+	// "github.com/iktefish/binary-helix/utils"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func TotalBasesOfEach() (int, int, int, int) {
+	genome, err := ioutil.ReadFile("test/input/escherichia-phage.fa")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    fmt.Println("gnome ~~> ", string(genome))
+
+	counts := make(map[string]int)
+
+	counts["A"] = 0
+	counts["C"] = 0
+	counts["G"] = 0
+	counts["T"] = 0
+
+    for _, base := range genome {
+        counts[string(base)] += 1
+    }
+
+    As := counts["A"]
+    Cs := counts["C"]
+    Gs := counts["G"]
+    Ts := counts["T"]
+
+    return As, Cs, Gs, Ts
+}
