84a283cb45ea5a135ccea1330ae5b4797e9ba655
iktefish <87042742+iktefish@users.noreply.github.com>
2022-04-23 16:44:54 +0600

    Implement complement.go, reverse-complement.go

---
+++ analyser/complement.go
@@ -0,0 +1,18 @@
+package analyser
+
+func Complement(s string) string {
+	complement := make(map[string]string)
+
+	complement["A"] = "T"
+	complement["C"] = "G"
+	complement["G"] = "C"
+	complement["T"] = "A"
+
+	c := ""
+
+	for _, base := range s {
+		c = c + complement[string(base)]
+	}
+
+	return c
+}
---
+++ analyser/reverse-complement.go
@@ -0,0 +1,18 @@
+package analyser
+
+func ReverseComplement(s string) string {
+	complement := make(map[string]string)
+
+	complement["A"] = "T"
+	complement["C"] = "G"
+	complement["G"] = "C"
+	complement["T"] = "A"
+
+	rc := ""
+
+	for _, base := range s {
+		rc = complement[string(base)] + rc
+	}
+
+	return rc
+}
