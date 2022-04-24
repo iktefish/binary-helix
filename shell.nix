00343eebefa4abab819b755128ab909ec9739cf6
iktefish <87042742+iktefish@users.noreply.github.com>
2022-04-24 10:46:36 +0600

    shell.nix: Add mongo-go-driver

--- shell.nix
+++ shell.nix
@@ -4,6 +4,10 @@ let
     fetchTarball
       https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
   pkgsUnstable = import unstableTarball { };
+  mongoGoDriver = buildGoModule {
+    src = fetchFromGitHub
+      { } + "github.com/mongodb/mongo-go-driver";
+  };
 
 in
 stdenv.mkDerivation {
